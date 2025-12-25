a7ac5a9fac0de3028a03e5b4af34791a6bc861c7
AbhimaniPG <167803632+ThathAbhi@users.noreply.github.com>
2025-12-25 11:29:11 +0530

    set " I.m a patient "option

--- src/pages/SignupOptionPage/optionPage.jsx
+++ src/pages/SignupOptionPage/optionPage.jsx
@@ -4,51 +4,45 @@ import { Link } from "react-router-dom";
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
 
-export default function OptionPage(){
-    return(
+export default function OptionPage() {
+  return (
     <div>
-        <div className="w-full  md:h-screen flex flex-col justify-center items-center gap-4 md:py-0 py-10 px-4">
-            <div className="md:mb-11 mb-5">
-                    <div className="md:text-3xl text-2xl font-bold text-center">
-                        <span className="text-secondary">Signup </span>Healthify As...
-                    </div>
-                    <div className="md:text-[20px] text-[15px] text-center">
-                        Select your account type to get started
-                    </div>
-            </div>
-            
-            <div className="flex md:flex-row flex-col gap-10 justify-center items-center">
-                    
-                            <OptionCard 
-                                icon={<FaUserDoctor />}
-                                title="I’m a Doctor"
-                                subtitle="Access patient records and provide better care"
-                                link="Register as Doctor"
-                                path="/doctor-register-1"
-                            />
-                            <OptionCard 
-                                icon={<FaUser  />}
-                                title="I’m a Patient"
-                                subtitle="Manage health records and track your wellness journey"
-                                link="Register as Patient"
-                                path="/form"
-                            />
-                            
-                    
-            </div>
-            <div className="md:text-sm text-[10px] text-center">
-                    Already have an account?
-                        <span className="text-secondary hover:underline">
-                            <Link to="/login" className="">
-                            Login here
-                            </Link>
-                        </span> 
-            </div>
-            
+      <div className="w-full  md:h-screen flex flex-col justify-center items-center gap-4 md:py-0 py-10 px-4">
+        <div className="md:mb-11 mb-5">
+          <div className="md:text-3xl text-2xl font-bold text-center">
+            <span className="text-secondary">Signup </span>Healthify As...
+          </div>
+          <div className="md:text-[20px] text-[15px] text-center">
+            Select your account type to get started
+          </div>
         </div>
 
-        <Footer/>
+        <div className="flex md:flex-row flex-col gap-10 justify-center items-center">
+          <OptionCard
+            icon={<FaUserDoctor />}
+            title="I’m a Doctor"
+            subtitle="Access patient records and provide better care"
+            link="Register as Doctor"
+            path="/doctor-register-1"
+          />
+          <OptionCard
+            icon={<FaUser />}
+            title="I’m a Patient"
+            subtitle="Manage health records and track your wellness journey"
+            link="Register as Patient"
+          />
+        </div>
+        <div className="md:text-sm text-[10px] text-center">
+          Already have an account?
+          <span className="text-secondary hover:underline">
+            <Link to="/login" className="">
+              Login here
+            </Link>
+          </span>
+        </div>
+      </div>
+
+      <Footer />
     </div>
-        
-    )
+  );
 }
