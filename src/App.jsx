6f29db95f43435da275197904487a0173e86bae1
AbhimaniPG <167803632+ThathAbhi@users.noreply.github.com>
2025-12-26 01:33:44 +0530

    add patient tegister pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import OptionPage from "./pages/SignupOptionPage/optionPage";
 import LoginPage from "./pages/LoginPage/loginPage";
@@ -8,23 +7,26 @@ import LoginPage from "./pages/LoginPage/loginPage";
 import PatientFormMain from "./pages/PatientFormPage/patientFormMain";
 import DoctorRegisterPage1 from "./pages/DoctorRegisterPage/DoctorRegisterPage1";
 import DoctorRegisterPage2 from "./pages/DoctorRegisterPage/DoctorRegisterPage2";
+import PatientRegisterPage1 from "./pages/PatientRegisterPage/PatientRegisterPage1";
+import PatientRegisterPage2 from "./pages/PatientRegisterPage/PatientRegisterPage2";
 
 function App() {
-  
-
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}/>
-       <Route path="/option" element={<OptionPage/>}/> 
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/form" element={<PatientFormMain />}/>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/option" element={<OptionPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/form" element={<PatientFormMain />} />
 
-      <Route path="/doctor-register-1" element={<DoctorRegisterPage1/>}/>
-      <Route path="/doctor-register-2" element={<DoctorRegisterPage2/>}/>
+      {/* Doctor Registration Routes */}
+      <Route path="/doctor-register-1" element={<DoctorRegisterPage1 />} />
+      <Route path="/doctor-register-2" element={<DoctorRegisterPage2 />} />
+
+      {/* Patient Registration Routes */}
+      <Route path="/patient-register-1" element={<PatientRegisterPage1 />} />
+      <Route path="/patient-register-2" element={<PatientRegisterPage2 />} />
     </Routes>
-  
-    
-  )
+  );
 }
 
-export default App
+export default App;
