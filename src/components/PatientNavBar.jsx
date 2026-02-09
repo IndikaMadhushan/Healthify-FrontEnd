0c5f02c79fb41656f8f8fd2983a59bec326e9b6a
AbhimaniPG <167803632+ThathAbhi@users.noreply.github.com>
2026-02-09 19:28:52 +0530

    export function PatientNavBar

--- src/components/PatientNavBar.jsx
+++ src/components/PatientNavBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function PatientNavBar({
+export function PatinetNavBar({
   patientData,
   // onLogout,
 }) {
