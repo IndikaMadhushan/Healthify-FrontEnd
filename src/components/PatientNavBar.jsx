662a994292f67f05ca1d8fa6f754a88382468f95
AbhimaniPG <167803632+ThathAbhi@users.noreply.github.com>
2026-02-09 19:12:10 +0530

    set PatinetNav as PatientNavBar

--- src/components/PatientNavBar.jsx
+++ src/components/PatientNavBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function PatinetNavBar({
+export function PatientNavBar({
   patientData,
   // onLogout,
 }) {
